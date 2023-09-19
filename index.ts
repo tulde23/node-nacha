8038472787a1684b69ef3e2740c9ea06307c9e27
Abourass <Abourassa@AssetVal.com>
2023-09-19 10:17:43 -0600

    :art: Tmep changes

---
+++ index.ts
@@ -0,0 +1,17 @@
+import Entry from './lib/entry/Entry.js';
+import EntryAddenda from './lib/entry-addenda/EntryAddenda.js';
+import Batch from './lib/batch/Batch.js';
+import File from './lib/file/File.js';
+import NACHParser from './lib/file/FileParser.js';
+import * as Utils from './lib/utils.js';
+import Validate from './lib/validate.js';
+
+export default {
+  Entry,
+  EntryAddenda,
+  Batch,
+  File,
+  NACHParser,
+  Utils,
+  Validate
+}
